4a5a3c044653ae64af5e223d81cb3977edcf95d3
alexc0re <nemshilov90@gmail.com>
2022-12-16 16:39:31 +0200

    added button test

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en or es")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_lang = request.config.getoption("language")
+    browser = None
+    if browser_lang:
+        print("\nstart ES chrome browser for test..")
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_lang})
+        browser = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
